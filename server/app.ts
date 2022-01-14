7386a603886dc833e7856c6da7194f3bec218703
ambiti0n <fabulousfresh010@gmail.com>
2022-01-14 15:47:22 +0100

    recipenodeapp

--- server/app.ts
+++ server/app.ts
@@ -1,20 +1,19 @@
-import express, { NextFunction, Request, Response } from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import mongoSanitize from 'express-mongo-sanitize';
-import cookieParser from 'cookie-parser';
-import mongoose from 'mongoose';
-import ErrorHandler from './utils/appError';
-import globalErrorHandler from './controllers/errorController';
+import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import ErrorHandler from "./utils/appError";
+import globalErrorHandler from "./controllers/errorController";
 
-import userRouter from './routes/userRoutes';
-import recipeRouter from './routes/recipeRoutes';
-import { connectTestDB } from './db/db';
+import userRouter from "./routes/userRoutes";
+import recipeRouter from "./routes/recipeRoutes";
+import { connectTestDB } from "./db/db";
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
 // Start express app
 const app = express();
@@ -23,13 +22,13 @@ const app = express();
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
@@ -42,12 +41,10 @@ app.use((req: any, res, next) => {
   next();
 });
 
-
-
-if (process.env.NODE_ENV === 'test') connectTestDB();
+if (process.env.NODE_ENV === "test") connectTestDB();
 else {
   const DB = (process.env.DATABASE as string).replace(
-    '<PASSWORD>',
+    "<PASSWORD>",
     process.env.DATABASE_PASSWORD as string
   );
 
@@ -62,14 +59,19 @@ else {
 }
 
 // 3) ROUTES
-app.use('/recipes', recipeRouter);
-app.use('/users', userRouter);
-// app.use('/restaurant', restaurantRouter);
+app.use("/recipes", recipeRouter);
+app.use("/users", userRouter);
 
-app.all('*', (req, res, next) => {
-  next(ErrorHandler(404, `Can't find ${req.originalUrl} on this server!`, {}));
+app.all("*", (req, res, next) => {
+  res.send(
+    `<h3>Can't find ${req.originalUrl} on this server, Register and login via a Post Request to /users/login. See postman documentation on the github for more information on the Api</h3>`
+  );
 });
+
+// app.all('*', (req, res, next) => {
+//   next (ErrorHandler(404, `Can't find ${req.originalUrl} on this server, login via a Post Request to /users/login. Visit postman documentation for more information`,  {}));
+// });
 
 app.use(globalErrorHandler);
 
- export default app
+export default app;
